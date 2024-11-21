2e92d8ac60c030d5f24f03cf0ef765e3b0eabbf2
Sharissa <sharissa0899@gmail.com>
2024-11-21 14:06:49 -0500

    Reset

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -7,15 +7,14 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-   public IActionResult Index()
-{
-    return View();
-}
+    public string Index()
+    {
+        return "This is my default action...";
+    }
     // 
     // GET: /HelloWorld/Welcome/ 
-// Requires using System.Text.Encodings.Web;
-public string Welcome(string name, int numTimes = 1)
-{
-    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
-}
+    public string Welcome()
+    {
+        return "This is the Welcome action method...";
+    }
 }
--- SharissaWebApp/obj/Debug/net8.0/SharissaWebApp.AssemblyInfo.cs
+++ SharissaWebApp/obj/Debug/net8.0/SharissaWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharissaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6606e4e87289351e9b90ec5a1dc438e92e62845a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18108c968351a4a8bcc51867f4d6e1e47a6b4775")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharissaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharissaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
