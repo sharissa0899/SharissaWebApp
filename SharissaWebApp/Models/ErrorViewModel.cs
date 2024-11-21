6606e4e87289351e9b90ec5a1dc438e92e62845a
Sharissa <sharissa0899@gmail.com>
2024-11-21 11:17:29 -0500

    testing Commit

---
+++ SharissaWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SharissaWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ SharissaWebApp/Models/Message.cs
@@ -0,0 +1,8 @@
+namespace SharissaWebApp.Models{
+
+    public class Message{
+        public int MessageID {get;set;}
+
+        public string MessageText{get;set;} =string.Empty;
+    }
+}
